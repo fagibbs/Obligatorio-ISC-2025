336a1b409cccbb9737965e3ea240852e7368eaed
fagibbs <facundo14123@gmail.com>
2025-11-23 19:10:02 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,12 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Ecommerce-Obligatorio"
+  type        = string
+  default     = "Ecommerce-Obligatorio"
+}
 
