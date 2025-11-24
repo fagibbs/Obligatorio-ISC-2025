4787bd6655239115adda574e08885d6b76308a6d
fagibbs <facundo14123@gmail.com>
2025-11-24 19:54:43 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,4 +9,9 @@ variable "project_name" {
   type        = string
   default     = "Ecommerce-Obligatorio-ISC"
 }
-
+variable "vpc" { default = "10.0.0.0/16" }
+variable "public_subnet" { default = "10.0.1.0/24" } 
+variable "private_app_az1" { default = "10.0.10.0/24" }
+variable "private_app_az2" { default = "10.0.11.0/24" }
+variable "private_db_az1" { default = "10.0.20.0/24" }
+variable "private_db_az2" { default = "10.0.21.0/24" }
