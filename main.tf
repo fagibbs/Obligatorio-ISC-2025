690965fef7d220582fb1bfb2912469c97812bd1a
Joaquín Manzanar <95896585+joacomanzanar9@users.noreply.github.com>
2025-11-24 21:50:42 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,27 @@
 module "network" {
-source = "./Network"
-vpc_cidr = var.vpc_cidr
+  source   = "./Network"
+  vpc_cidr = var.vpc_cidr
 }
-
-
-module "computers" {
-source = "./Computers"
-vpc_id = module.network.vpc_id
-app_subnets = module.network.app_subnets
-sg_alb_id = module.services.sg_alb_id
-key_name = var.key_name
-}
-
 
 module "services" {
-source = "./Services"
-vpc_id = module.network.vpc_id
-public_subnets = module.network.public_subnets
-sg_app_id = module.computers.sg_app_id
+  source         = "./Services"
+  vpc_id         = module.network.vpc_id
+  public_subnets = module.network.public_subnets
 }
 
+module "computers" {
+  source            = "./Computers"
+  vpc_id            = module.network.vpc_id
+  app_subnets       = module.network.app_subnets
+  sg_alb_id         = module.services.sg_alb_id
+  target_group_arn  = module.services.target_group_arn
+  key_name          = var.key_name
+}
 
 module "db" {
-source = "./DB"
-vpc_id = module.network.vpc_id
-db_subnets = module.network.db_subnets
-sg_app_id = module.computers.sg_app_id
-key_name = var.key_name
+  source     = "./DB"
+  vpc_id     = module.network.vpc_id
+  db_subnets = module.network.db_subnets
+  sg_app_id  = module.computers.sg_app_id
+  key_name   = var.key_name
 }
