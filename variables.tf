b68e4b8108a468e94a113d97629fca5846b7f2c9
Joaquín Manzanar <95896585+joacomanzanar9@users.noreply.github.com>
2025-11-24 21:53:27 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,64 @@
 variable "aws_region" {
-  description = "AWS region"
+  description = "Región AWS donde se desplegará la infraestructura"
   type        = string
   default     = "us-east-1"
 }
 
 variable "project_name" {
-  description = "Ecommerce-Obligatorio-ISC"
+  description = "Nombre del proyecto"
   type        = string
   default     = "Ecommerce-Obligatorio-ISC"
 }
-variable "vpc" { default = "10.0.0.0/16" }
-variable "public_subnet" { default = "10.0.1.0/24" } 
-variable "private_app_az1" { default = "10.0.10.0/24" }
-variable "private_app_az2" { default = "10.0.11.0/24" }
-variable "private_db_az1" { default = "10.0.20.0/24" }
-variable "private_db_az2" { default = "10.0.21.0/24" }
+
+variable "vpc_cidr" {
+  description = "CIDR principal de la VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_a" {
+  description = "Subnet pública AZ us-east-1a"
+  type        = string
+  default     = "10.0.0.0/24"
+}
+
+variable "public_subnet_b" {
+  description = "Subnet pública AZ us-east-1b"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "private_app_az1" {
+  description = "Subnet privada App AZ1"
+  type        = string
+  default     = "10.0.10.0/24"
+}
+
+variable "private_app_az2" {
+  description = "Subnet privada App AZ2"
+  type        = string
+  default     = "10.0.11.0/24"
+}
+
+variable "private_db_az1" {
+  description = "Subnet privada DB AZ1"
+  type        = string
+  default     = "10.0.20.0/24"
+}
+
+variable "private_db_az2" {
+  description = "Subnet privada DB AZ2"
+  type        = string
+  default     = "10.0.21.0/24"
+}
+
+variable "instance_type" {
+  description = "Tipo de instancia para servidores APP"
+  type        = string
+  default     = "t3.small"
+}
+
+variable "key_name" {
+  description = "Nombre del key pair existente en AWS"
+  type        = string
+}
