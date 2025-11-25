996f153cff9fe720aaa38b9a877e5e17ae054e7f
Joaquín Manzanar <95896585+joacomanzanar9@users.noreply.github.com>
2025-11-24 21:48:38 -0300

    Update outputs.tf
    Agrego descripción a los outputs

--- Terraform/Network/outputs.tf
+++ Terraform/Network/outputs.tf
@@ -1,4 +1,19 @@
-output "vpc_id" { value = aws_vpc.main.id }
-output "public_subnets" { value = [aws_subnet.public_a.id, aws_subnet.public_b.id] }
-output "app_subnets" { value = [aws_subnet.app_a.id, aws_subnet.app_b.id] }
-output "db_subnets" { value = [aws_subnet.db_a.id, aws_subnet.db_b.id] }
+output "vpc_id" {
+  description = "ID de la VPC principal"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnets" {
+  description = "Subnets públicas (ALB y NAT)"
+  value       = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+}
+
+output "app_subnets" {
+  description = "Subnets privadas para servidores de aplicación"
+  value       = [aws_subnet.app_a.id, aws_subnet.app_b.id]
+}
+
+output "db_subnets" {
+  description = "Subnets privadas para base de datos"
+  value       = [aws_subnet.db_a.id, aws_subnet.db_b.id]
+}
