75377922379a146a64fc0758b2d10a1ab963f21d
Joaquín Manzanar <95896585+joacomanzanar9@users.noreply.github.com>
2025-11-24 22:00:56 -0300

    Update data.tf

--- Terraform/data.tf
+++ Terraform/data.tf
@@ -1,10 +1,19 @@
 data "aws_ami" "amazon_linux" {
-most_recent = true
-owners = ["amazon"]
+  most_recent = true
+  owners      = ["amazon"]
 
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
 
-filter {
-name = "name"
-values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-}
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
 }
