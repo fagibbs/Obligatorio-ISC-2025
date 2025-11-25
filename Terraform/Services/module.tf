dd80c846cdee26bb86d5ff21d705722764d72e29
Joaquín Manzanar <95896585+joacomanzanar9@users.noreply.github.com>
2025-11-24 21:49:52 -0300

    Update module.tf

--- Terraform/Services/module.tf
+++ Terraform/Services/module.tf
@@ -1,37 +1,59 @@
 resource "aws_security_group" "sg_alb" {
-ingress {
-from_port = 80
-to_port = 80
-protocol = "tcp"
-cidr_blocks = ["0.0.0.0/0"]
-}
-}
+  name   = "sg-alb"
+  vpc_id = var.vpc_id
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 resource "aws_lb" "alb" {
-name = "alb-ecommerce"
-load_balancer_type = "application"
-security_groups = [aws_security_group.sg_alb.id]
-subnets = var.public_subnets
+  name               = "alb-ecommerce"
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.sg_alb.id]
+  subnets            = var.public_subnets
 }
-
 
 resource "aws_lb_target_group" "tg" {
-name = "tg-ecommerce"
-port = 80
-protocol = "HTTP"
-vpc_id = var.vpc_id
-}
+  name     = "tg-ecommerce"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
 
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+}
 
 resource "aws_lb_listener" "listener" {
-load_balancer_arn = aws_lb.alb.arn
-port = 80
-protocol = "HTTP"
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
 
-
-default_action {
-type = "forward"
-target_group_arn = aws_lb_target_group.tg.arn
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
 }
+
+output "sg_alb_id" {
+  value = aws_security_group.sg_alb.id
+}
+
+output "target_group_arn" {
+  value = aws_lb_target_group.tg.arn
 }
