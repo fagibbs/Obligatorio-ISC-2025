ba771c0b05168f54daaa08bb58a9f200840514bc
fagibbs <facundo14123@gmail.com>
2025-11-28 19:00:49 -0300

    agregue variables para VPC y sus subnets

---
+++ Terraform/Network/variables.tf
@@ -0,0 +1,8 @@
+variable "vpc_cidr" { type = string }
+variable "public_subnet_a" { type = string }
+variable "public_subnet_b" { type = string }
+variable "private_app_az1" { type = string }
+variable "private_app_az2" { type = string }
+variable "private_db_az1" { type = string }
+variable "private_db_az2" { type = string }
+variable "region" { type = string }
