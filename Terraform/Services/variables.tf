c03d7de73a312c29952fc3bbcd8a0a0d11613cf6
Joaquín Manzanar <95896585+joacomanzanar9@users.noreply.github.com>
2025-11-29 16:39:31 -0300

    Se comenta variables.tf

--- Terraform/Services/variables.tf
+++ Terraform/Services/variables.tf
@@ -1,7 +1,9 @@
+# ID de la VPC donde se crean el ALB y sus recursos asociados
 variable "vpc_id" {
   type = string
 }
 
+# Lista de subnets públicas donde se despliega el ALB
 variable "public_subnets" {
   type = list(string)
 }
