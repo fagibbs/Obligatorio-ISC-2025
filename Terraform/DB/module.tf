77b5aa1117ad3edf59a92ed986f056f15c3ba917
Joaquín Manzanar <95896585+joacomanzanar9@users.noreply.github.com>
2025-11-29 16:32:12 -0300

    Se comenta module.tf

--- Terraform/DB/module.tf
+++ Terraform/DB/module.tf
@@ -1,7 +1,9 @@
+# Security Group de la "base de datos" (La EC2 que simula el RDS)
 resource "aws_security_group" "sg_db" {
   name   = "db-sg"
   vpc_id = var.vpc_id
 
+# Regla de entrada: permiitir MySQL (3306) solo desde las instancias de aplicación
   ingress {
     from_port       = 3306
     to_port         = 3306
@@ -9,6 +11,7 @@ resource "aws_security_group" "sg_db" {
     security_groups = [var.sg_app_id]
   }
 
+# Regla de salida: permite todo tráfico saliente (para actualizar paquetes y demás)
   egress {
     from_port = 0
     to_port   = 0
@@ -22,6 +25,7 @@ resource "aws_db_subnet_group" "db_subnets" {
   subnet_ids = var.db_subnets
 }
 
+# Instancia DB
 resource "aws_db_instance" "mysql" {
   identifier           = "ecommerce-db"
   engine               = "mysql"
