ca49f3d439a10f66436429f6d715cc5bfd20056b
fagibbs <facundo14123@gmail.com>
2025-11-30 15:20:34 -0300

    agregue todos los outputs que estaban en module

--- Terraform/Services/outputs.tf
+++ Terraform/Services/outputs.tf
@@ -1,3 +1,11 @@
+output "sg_alb_id" {
+  value = aws_security_group.sg_alb.id
+}
+
 output "target_group_arn" {
   value = aws_lb_target_group.tg.arn
+}
+
+output "alb_dns_name" {
+  value = aws_lb.alb.dns_name
 }
