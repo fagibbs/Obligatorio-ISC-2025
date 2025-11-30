f7ca9284c95365908985c7d55eaef81bbff4e16f
fagibbs <facundo14123@gmail.com>
2025-11-30 15:20:05 -0300

    saque los outputs
    Removed output variables for security group ID, target group ARN, and ALB DNS name.

--- Terraform/Services/module.tf
+++ Terraform/Services/module.tf
@@ -88,17 +88,6 @@ resource "aws_lb_listener" "listener" {
   }
 }
 
-output "sg_alb_id" {
-  value = aws_security_group.sg_alb.id
-}
-
-output "target_group_arn" {
-  value = aws_lb_target_group.tg.arn
-}
-
-output "alb_dns_name" {
-  value = aws_lb.alb.dns_name
-}
 
 # Alarma que detecta cuando el ALB se queda sin instancias saludables en el Target Group
 resource "aws_cloudwatch_metric_alarm" "alb_no_healthy_targets" {
