2ed2a3ec4e9ab9fcbe28a0b715c557c7be475692
fagibbs <facundo14123@gmail.com>
2025-12-02 14:11:12 -0300

    Permitimos todo el tráfico saliente en las ec2

--- Terraform/Computers/module.tf
+++ Terraform/Computers/module.tf
@@ -13,9 +13,9 @@ resource "aws_security_group" "sg_app" {
 
 # Salida: permite todo tráfico saliente (pensando en el acceso a Internet vía NAT)
   egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
